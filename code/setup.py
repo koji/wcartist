1bd354c6acc3d073a400d6643c138598a9b1ddeb
koji <kk2796@nyu.edu>
2020-11-26 03:17:17 -0500

    fix file path issue

--- code/setup.py
+++ code/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="wcartist",
       version="0.0.1",
       description="wcartist is a module to create a word cloud image.",
-      long_description=open('readme.md').read(),
+      long_description=open('../README.md').read(),
       long_description_content_type="text/markdown",
       url="https://github.com/koji/wcartist",
       author='koji kanao',
